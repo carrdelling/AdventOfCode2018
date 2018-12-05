8724725393a379282bda2abd49f9d1f20f4fab13
Joaquin Derrac Rus <carrdelling@gmail.com>
2018-12-05 00:00:51 +0000

    Move solution to the bottom

--- p1/solve.py
+++ p1/solve.py
@@ -5,8 +5,6 @@ with open('input_data') as in_f:
         val = int(row.strip())
         frequencies.append(val)
 
-print(f'1_1: {sum(frequencies)}')
-
 seen = set()
 current = 0
 seen.add(current)
@@ -20,5 +18,5 @@ while True:
     seen.add(current)
     idx = (idx + 1) % len(frequencies)
 
+print(f'1_1: {sum(frequencies)}')
 print(f'1_2: {current}')
-
